88aaeb71531ba31d5cfa83a2a713bef0578f6a78
Rodolfo Chagas <nsc.rodolfo@gmail.com>
2020-07-09 17:09:54 -0300

    Finalização de geração de gráficos/relatórios (falta tratar condicionais caso não hajam tais relatórios

--- view/Pragas.php
+++ view/Pragas.php
@@ -61,6 +61,8 @@ $Propriedade = mysqli_fetch_array($result4);
   <link href="../assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!-- Bootstrap core CSS -->
   <link href="../assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.21/b-1.6.2/b-flash-1.6.2/b-html5-1.6.2/b-print-1.6.2/r-2.2.5/datatables.min.css" />
+
 
 
   <!-- Additional CSS Files -->
@@ -112,7 +114,7 @@ $Propriedade = mysqli_fetch_array($result4);
                   Nome Científico: ' . $tupla['Nome'] . '
                 </span>
                 <br>
-                <a href="#" onClick="showDiv();">
+                <a href="#" onClick="showDiv(\'' . $tupla['Nome'] . '\' , \'' . $tupla['Cod_Praga'] . '\');">
                 Gerar relatórios
                 </a>
                 <a href="excluirPraga.php?idPraga=' . $tupla['Cod_PresencaPraga'] . '&CodTalhao=' . $codTalhao . '&CodCultura=' . $codCultura . '" class="btn btn-secondary bec" >Excluir</a>
@@ -202,6 +204,74 @@ $Propriedade = mysqli_fetch_array($result4);
     </div>
   </div>
 
+  <div class="modal fade" id="modalGraficoPragaContagem" tabindex="-1" role="dialog" aria-labelledby="modalGraficoPragaContagemLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalGraficoPragaContagemLabel"></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <canvas id="chartGraficoContagem"></canvas>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="modalGraficoPragaAplicacao" tabindex="-1" role="dialog" aria-labelledby="modalGraficoPragaAplicacaoLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalGraficoPragaAplicacaoLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <canvas id="chartGraficoAplicacao"></canvas>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="modalPdfExcelPlanos" tabindex="-1" role="dialog" aria-labelledby="modalPdfExcelPlanosLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalPdfExcelPlanosLabel"></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <table id="tabelaPlanos" class="display">
+
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="modalPdfExcelAplicacoes" tabindex="-1" role="dialog" aria-labelledby="modalPdfExcelAplicacoesLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalPdfExcelAplicacoesLabel"></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <table id="tabelaAplicacoes" class="display">
+        </div>
+      </div>
+    </div>
+  </div>
+
   <!-- Scripts -->
   <!-- Bootstrap core JavaScript -->
   <script src="../assets/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -211,18 +281,74 @@ $Propriedade = mysqli_fetch_array($result4);
   <script src="../assets/js/transition.js"></script>
   <script src="../assets/js/owl-carousel.js"></script>
   <script src="../assets/js/custom.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.21/b-1.6.2/b-flash-1.6.2/b-html5-1.6.2/b-print-1.6.2/r-2.2.5/datatables.min.js"></script>
+
   <script>
-// $Cod_Talhao = $_GET['Cod_Talhao'];
-// $Cod_Praga = $_GET['Cod_Praga'];
-    var codTalhao = <?php echo $codTalhao ?>
-    var codPraga = 0;//vai vir na hora que chama a função
+    // $Cod_Talhao = $_GET['Cod_Talhao'];
+    // $Cod_Praga = $_GET['Cod_Praga'];
+    var codTalhao = <?php echo $codTalhao ?>;
+    var codPraga; //vai vir na hora que chama a função
+    var nomePraga;
+    var nomePlanta = "<?php echo $Planta['Nome']; ?>";
+
+    function showDiv(res_nomePraga, res_codPraga) {
+      codPraga = res_codPraga;
+      nomePraga = res_nomePraga;
+      // alert("cod praga eh " + codPraga);
+      $('#modalRelatorios').modal('show');
+      $('#exampleModalLabel').text('MIP² | Relatórios <?php echo $Planta['Nome']; ?>: <?php echo $Talhao['Nome'] ?>');
+      $('#modalGraficoPragaContagemLabel').text('MIP² | Relatório <?php echo $Planta['Nome']; ?>' + " x " + nomePraga);
+      $('#modalGraficoPragaAplicacaoLabel').text('MIP² | Relatório <?php echo $Planta['Nome']; ?>' + " x " + nomePraga);
+
+    }
+
     $('#graficoPragaContagem').click(function() {
       $.ajax({
         type: "GET",
         url: "../apis/resgataDadosGraphPlantasPlanos.php",
-        data: {"Cod_Talhao" = , "Cod_Praga" = };
+        data: {
+          "Cod_Talhao": codTalhao,
+          "Cod_Praga": codPraga
+        },
         success: function(result) {
-          //gera o gráfico
+          dados = JSON.parse(result);
+
+          //verificar se -> existe dados[0]['Data'])
+          $('#modalRelatorios').modal('hide');
+          $('#modalGraficoPragaContagem').modal('show');
+          $('#modalGraficoPragaContagemLabel').text('MIP² | ' + nomePlanta + " x " + nomePraga);
+          let datas = [];
+          let pop_pragas = [];
+          //prepara os dados de entrada do gráfico
+          for (var key in dados) {
+            dataE = (dados[key].Data).split("-")
+            datex = dataE[2] + "/" + dataE[1] + "/" + dataE[0];
+            datas.push(datex);
+            pop_pragas.push(dados[key].popPragas);
+          }
+
+          var ctx = document.getElementById('chartGraficoContagem').getContext('2d');
+          var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+              labels: [...datas],
+              datasets: [{
+                label: 'População de pragas',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: pop_pragas
+              }]
+            },
+
+            // Configuration options go here
+            options: {}
+          });
         },
         error: function() {
           //
@@ -236,8 +362,45 @@ $Propriedade = mysqli_fetch_array($result4);
       $.ajax({
         type: "GET",
         url: "../apis/resgataDadosGraphAplicacao.php",
+        data: {
+          "Cod_Talhao": codTalhao,
+          "Cod_Praga": codPraga
+        },
         success: function(result) {
-          //gera o gráfico
+          dados = JSON.parse(result);
+
+          //verificar se -> existe dados[0]['Data'])
+          $('#modalRelatorios').modal('hide');
+          $('#modalGraficoPragaAplicacao').modal('show');
+
+          let datas = [];
+          let pop_pragas = [];
+          //prepara os dados de entrada do gráfico
+          for (var key in dados) {
+            dataE = (dados[key].Data).split("-")
+            datex = dataE[2] + "/" + dataE[1] + "/" + dataE[0];
+            datas.push(datex);
+            pop_pragas.push(dados[key].popPragas);
+          }
+
+          var ctx = document.getElementById('chartGraficoAplicacao').getContext('2d');
+          var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+            // The data for our dataset
+            data: {
+              labels: [...datas],
+              datasets: [{
+                label: 'População de pragas',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: pop_pragas
+              }]
+            },
+
+            // Configuration options go here
+            options: {}
+          });
         },
         error: function() {
           //
@@ -246,42 +409,162 @@ $Propriedade = mysqli_fetch_array($result4);
       return false;
     });
 
-   
-    $('#listaPdfExcelPlanos').click(function() {
-      $.ajax({
-        type: "GET",
-        url: "../apis/resgataDadosGraphPlantasPlanos.php",
-        success: function(result) {
-          //gera abre datatable com os dados 
-        },
-        error: function() {
-          //
-        }
+    $(document).ready(function() {
+      $('#listaPdfExcelPlanos').click(function() {
+        $.ajax({
+          type: "GET",
+          url: "../apis/resgataDadosGraphPlantasPlanos.php",
+          data: {
+            "Cod_Talhao": codTalhao,
+            "Cod_Praga": codPraga
+          },
+          success: function(result) {
+            var dados = JSON.parse(result);
+            console.log(dados);
+
+            var dadosLocal = [];
+            var dadosGeral = [];
+            for (var key in dados) {
+              var dataE = (dados[key].Data).split("-")
+              var datex = dataE[2] + "/" + dataE[1] + "/" + dataE[0];
+              dadosLocal.push(datex);
+              var autor = dados[key].Autor;
+              dadosLocal.push(autor);
+              var popPragas = dados[key].popPragas;
+              dadosLocal.push(popPragas);
+              var numPlantas = dados[key].numPlantas;
+              dadosLocal.push(numPlantas);
+              var cultura = "<?php echo $Planta['Nome'] ?>";
+              dadosLocal.push(cultura);
+              var talhao = "<?php echo $Talhao['Nome'] ?>";
+              dadosLocal.push(talhao);
+              dadosGeral.push(dadosLocal);
+            }
+            // alert(dadosGeral);
+            var dataSet = dadosGeral;
+
+            //Lista o Autor, Cultura, Talhão, Data do plano, plantas infestadas, número de amostras
+            dados = JSON.parse(result);
+            $('#modalRelatorios').modal('hide');
+            $('#modalPdfExcelPlanos').modal('show');
+
+            $('#tabelaPlanos').DataTable({
+              dom: 'Bfrtip',
+              buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+              ],
+              responsive: true,
+              data: dataSet,
+              columns: [{
+                  title: "Data"
+                },
+                {
+                  title: "Autor"
+                },
+                {
+                  title: "Pop. pragas"
+                },
+                {
+                  title: "Num. plantas"
+                },
+                {
+                  title: "Cultura"
+                },
+                {
+                  title: "Talhão"
+                }
+              ]
+            });
+            //gera o datatable
+          },
+          error: function() {
+            //
+          }
+        });
+        return false;
       });
-      return false;
     });
 
-    $('#listaPdfExcelAplicacoes').click(function() {
-      $.ajax({
-        type: "GET",
-        url: "../apis/resgataDadosGraphAplicacao.php",
-        success: function(result) {
-          //abre datatable com os dados
-        },
-        error: function() {
-          //
-        }
+    $(document).ready(function() {
+      $('#listaPdfExcelAplicacoes').click(function() {
+        $.ajax({
+          type: "GET",
+          url: "../apis/resgataDadosGraphAplicacao.php",
+          data: {
+            "Cod_Talhao": codTalhao,
+            "Cod_Praga": codPraga
+          },
+          success: function(result) {
+            var dados = JSON.parse(result);
+            console.log(dados);
+            // alert(dados);
+            //Lista o Autor, Cultura, Talhão, Método Aplicado, 
+            //Data da aplicação, plantas infestadas, número de amostras
+            var dadosLocal = [];
+            var dadosGeral = [];
+            for (var key in dados) {
+              var dataE = (dados[key].Data).split("-")
+              var datex = dataE[2] + "/" + dataE[1] + "/" + dataE[0];
+              dadosLocal.push(datex);
+              var autor = dados[key].Autor;
+              dadosLocal.push(autor);
+              var numAmostras = dados[key].popPragas;
+              dadosLocal.push(numAmostras);
+              var numPlantas = dados[key].numPlantas;
+              dadosLocal.push(numPlantas);
+              var cultura = "<?php echo $Planta['Nome'] ?>";
+              dadosLocal.push(cultura);
+              var talhao = "<?php echo $Talhao['Nome'] ?>";
+              dadosLocal.push(talhao);
+              dadosGeral.push(dadosLocal);
+            }
+            // alert(dadosGeral);
+            var dataSet = dadosGeral;
+
+            //Lista o Autor, Cultura, Talhão, Data do plano, plantas infestadas, número de amostras
+            dados = JSON.parse(result);
+            $('#modalRelatorios').modal('hide');
+            $('#modalPdfExcelAplicacoes').modal('show');
+
+            $('#tabelaAplicacoes').DataTable({
+              dom: 'Bfrtip',
+              buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+              ],
+              responsive: true,
+              data: dataSet,
+              columns: [{
+                  title: "Data"
+                },
+                {
+                  title: "Autor"
+                },
+                {
+                  title: "Cultura"
+                },
+                {
+                  title: "Talhão"
+                },
+                {
+                  title: "Método"
+                },
+                {
+                  title: "Plantas infestadas"
+                },
+                {
+                  title: "Num. de amostras"
+                }
+              ]
+            });
+            //gera o datatable
+          },
+          error: function() {
+            //
+          }
+        });
+        return false;
       });
-      return false;
     });
-
- 
-
-
-    function showDiv(nomePraga) {
-      $('#modalRelatorios').modal('show');
-      $('#exampleModalLabel').text('MIP² | Relatório <?php echo $Planta['Nome']; ?> ' + nomePraga);
-    }
   </script>
 
 </html>
